d42a33db76f2d2cb2b3a3a8aaf8b5eacd6cfbd09
Vitalii Lototskyi <vlototskyi@icloud.com>
2020-02-03 01:17:26 +0200

    Implemented proof of work

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/vlototskyi/ergo-blockchain/blockchain"
 )
@@ -12,10 +13,16 @@ func main() {
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
+	chain.AddBlock("Fourth Block after Genesis")
+	chain.AddBlock("Fifth Block after Genesis")
 
 	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
